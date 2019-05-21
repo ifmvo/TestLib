270a9ff365d292fb2f27ad9e6aada8603b59994e
Matthew_Chen <Matthew_Chen_1994@163.com>
2019-05-21 08:59:33 +0800

    Test Github repositories

--- app/src/main/java/com/ifmvo/gittest/MainActivity.kt
+++ app/src/main/java/com/ifmvo/gittest/MainActivity.kt
@@ -2,12 +2,15 @@ package com.ifmvo.gittest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.ifmvo.testlibrary.Utils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Utils.add()
     }
 
 }
